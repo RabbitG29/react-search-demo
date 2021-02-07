f80148234da60180965ca6c5b5ea48828f04305a
KDH <recoder1212@naver.com>
2021-02-07 21:16:56 +0900

    added handling loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Search from './search/container/Search'
 import User from './user/container/User'
 import 'antd/dist/antd.css'
 import { Route } from 'react-router-dom';
 
 export default function App() {
+  useEffect(() => {
+    const bodyEl = document.getElementsByTagName('body')[0];
+    const loadingEl = document.getElementById('init-loading');
+    bodyEl.removeChild(loadingEl);
+  }, []);
   return (
     <>
       <Route exact path="/" component={Search} />
